936e10e4925947ab35fbe004d3d51f88f53fac2c
practicantesigips <practicante.sig.ips@gmail.com>
2025-08-11 14:16:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ---------- CONFIG ----------
 const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTMnyquRcVFWwJgETIp4kOW3F4U0o1aDCS4KK76jYSdoLMdzbAu-VmZRilwUneR4Xjh4GLqLPh5ALfO/pub?output=csv";
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwcYh3f2iniRdACTDH6BCvzKAJ0YvKjxSNC7OtfqsWZJ1RnDE-oLNmP1Ty_zyBpAT8s/exec"; // <--- cambia esto
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyVHw2_v0-w6I4ZRTC8NpmSRnrEM625FoGxIjOu_g7qKsdpOnrsaqjXUChf1o8Wdbak/exec"; // <--- cambia esto
 
 // ---------- UTIL: parse CSV robusto (maneja comillas) ----------
 function parseCSV(text) {
@@ -285,6 +285,7 @@ formEl.addEventListener('submit', async (ev)=>{
 
   btnEnviar.disabled = false;
 });
+
 
 
 
